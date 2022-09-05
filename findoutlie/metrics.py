1ca2b311cdb424a266661b7f099c5fc9185b2486
RDoerfel <doerfelruben@aol.com>
2022-09-05 19:47:48 +0200

    Implement dvars for each volume in 4D image

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -2,8 +2,7 @@
 """
 
 # Any imports you need
-# +++your code here+++
-
+import numpy as np
 
 def dvars(img):
     """ Calculate dvars metric on Nibabel image `img`
@@ -21,12 +20,8 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumes in `img`.
     """
-    # Hint: remember 'axis='.  For example:
-    # In [2]: arr = np.array([[2, 3, 4], [5, 6, 7]])
-    # In [3]: np.mean(arr, axis=1)
-    # Out[2]: array([3., 6.])
-    #
-    # You may be be able to solve this in four lines, without a loop.
-    # But solve it any way you can.
-    # This is a placeholder, replace it to write your solution.
-    raise NotImplementedError('Code up this function')
+    data = img.get_fdata()
+    n_voxels = data.shape[0] * data.shape[1] * data.shape[2]
+    data_2d = data.reshape(n_voxels, data.shape[-1])
+    dvals = np.sqrt(np.mean(np.square(np.diff(data_2d, axis=1)), axis=0))
+    return dvals
