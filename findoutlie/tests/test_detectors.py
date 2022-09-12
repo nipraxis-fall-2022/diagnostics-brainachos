8a241c19f071c59c54acbed578b766d0a6df6a7c
RDoerfel <doerfelruben@aol.com>
2022-09-12 18:20:10 +0200

    Enh: implement detectors function and test
    use interquartile range to detect outliers

--- findoutlie/tests/test_detectors.py
+++ findoutlie/tests/test_detectors.py
@@ -23,19 +23,27 @@ import numpy as np
 from detectors import iqr_detector
 
 
-def test_iqr_detector():
+def test_iqr_detector_default():
     # From: http://www.purplemath.com/modules/boxwhisk3.htm
     example_values = np.array(
         [10.2, 14.1, 14.4, 14.4, 14.4, 14.5, 14.5, 14.6, 14.7, 14.7, 14.7,
          14.9, 15.1, 15.9, 16.4])
     is_outlier = iqr_detector(example_values, 1.5)
+
     assert np.all(example_values[is_outlier] == [10.2, 15.9, 16.4])
+
+def test_iqr_detector_not_default():
     # Test not-default value for outlier proportion
+    example_values = np.array(
+    [10.2, 14.1, 14.4, 14.4, 14.4, 14.5, 14.5, 14.6, 14.7, 14.7, 14.7,
+         14.9, 15.1, 15.9, 16.4])
     is_outlier = iqr_detector(example_values, 0.5)
     assert np.all(example_values[is_outlier] == [10.2, 14.1, 15.1, 15.9, 16.4])
 
 
 if __name__ == '__main__':
     # File being executed as a script
-    test_iqr_detector()
+    test_iqr_detector_default()
+    test_iqr_detector_not_default()
+
     print('Tests passed')
